a3aaa192f8b0b6bdf5cfb13a5c5d962727971b18
Anna Lou Parejo <aparejo0984@gmail.com>
2023-11-12 21:05:23 -0500

    Add default avatar for newly added users

--- src/utils/imageAssets.js
+++ src/utils/imageAssets.js
@@ -2,6 +2,7 @@ const images = {
 	"ann.png": require("../assets/img/ann.png"),
 	"gomi.jpg": require("../assets/img/gomi.jpg"),
 	"admin1.jpg": require("../assets/img/admin1.jpg"),
+	"avatar-icon.jpeg": require("../assets/img/avatar-icon.jpeg")
 };
 
 export default images;
